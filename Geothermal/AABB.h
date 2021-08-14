2527b4ab82c5b8360e7087a91acb0d5c740ae565
Xiao <chairmantoad@gmail.com>
2021-08-15 03:02:06 +0800

    Have the mesh carries its bounds

--- Geothermal/AABB.h
+++ Geothermal/AABB.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "pch.h"
 
+// AABB is used extensively outside of just the graphics module
+// thus it belongs here with the rest of the ultilities
+
 /// <summary>
 /// Axis-Aligned Bounding Box
 /// </summary>
