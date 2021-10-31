0df19df745ca85f806c153f070385ea0f0771634
Xiao <cuoshen@g.ucla.edu>
2021-10-31 15:01:17 -0700

    First draft of PoolAllocator

--- Geothermal/PoolAllocator.cpp
+++ Geothermal/PoolAllocator.cpp
@@ -5,14 +5,32 @@ using namespace Geothermal;
 
 void* PoolAllocator::Allocate()
 {
-	return nullptr;
+	if (nextAllocationPosition == nullptr)
+	{
+		nextAllocationPosition = AllocateBlock();
+	}
+
+	Chunk* freeChunk = nextAllocationPosition;
+	nextAllocationPosition = nextAllocationPosition->next;
+
+	return freeChunk;
 }
 
 void PoolAllocator::Deallocate(void* object)
 {
+	reinterpret_cast<Chunk*>(object)->next = nextAllocationPosition;
+	nextAllocationPosition = reinterpret_cast<Chunk*>(object);
 }
 
 PoolAllocator::Chunk* PoolAllocator::AllocateBlock()
 {
-	return nullptr;
+	size_t blockSize = chunksPerBlock * chunkSize;
+	Chunk* begin = reinterpret_cast<Chunk*>(malloc(blockSize));
+	Chunk* chunk = begin;
+	for (int i = 0; i < chunksPerBlock - 1; i++)
+	{
+		chunk->next = reinterpret_cast<Chunk*>(reinterpret_cast<char*>(chunk) + chunkSize);
+		chunk = chunk->next;
+	}
+	return begin;
 }
